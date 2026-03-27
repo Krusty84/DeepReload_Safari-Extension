81f9c6a6198a8fc037b870a5025709782b1b39ca
Alexey Sedoykin <sedoykin@gmail.com>
2026-03-27 22:54:20 +0300

    Initial Commit

---
+++ DeepReload/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  DeepReload
+//
+//  Created by Sedoykin Alexey on 27/03/2026.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
